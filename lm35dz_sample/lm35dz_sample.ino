bc2d3fac8bb46bae5879eef6240b0f401f99f365
sIn <ssaito.m2m@gmail.com>
2025-01-20 15:30:15 +0900

    commit

---
+++ lm35dz_sample/lm35dz_sample.ino
@@ -0,0 +1,13 @@
+float analogIn;          
+float temp = 0;  
+
+void setup(){
+  Serial.begin(9600);  
+}
+
+void loop(){
+  analogIn=analogRead(0);     // A0ピンからセンサ値を取得
+  temp=((5*analogIn)/1024)*100;     // センサ値を摂氏に換算
+  Serial.println(temp);     // シリアルモニタに表示
+  delay(1000);      // 1000ms待機
+}
